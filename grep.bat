db1b896b424a568b5cfebf7d0196ee484baea2ee
Havsalt <knutolaihelgesen@gmail.com>
2024-06-18 21:32:54 +0200

    Updated run script

--- grep.bat
+++ grep.bat
@@ -1,8 +1,17 @@
 @echo off
+
+@REM == Batch script for running python command line script ==
+@REM Activates the project's .venv
+@REM Runs script as python module
+@REM Exits with script's exit code
+
 call %~dp0\.venv\Scripts\activate.bat
 python -m grep %*
 
-@REM deactivate script from .venv\Scripts\deactivate.bat
+
+@REM The following part is from `deactivate.bat`
+@REM It is usually found in .venv\Scripts\deactivate.bat
+
 @set VIRTUAL_ENV=
 @set VIRTUAL_ENV_PROMPT=
 
@@ -22,4 +31,5 @@ python -m grep %*
     @set _OLD_VIRTUAL_PATH=
 :ENDIFVPATH
 
+@REM Bubble up exit code from script, to parent process
 exit /b %ERRORLEVEL%
