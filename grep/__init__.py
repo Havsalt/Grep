3f9dae4d1d67c67e34ac3f2d7de7801e15d87bd0
Havsalt <knutolaihelgesen@gmail.com>
2024-06-18 21:18:49 +0200

    Refactored

--- grep/__init__.py
+++ grep/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 import argparse
 import pathlib
@@ -6,13 +6,15 @@ import re
 from typing import Generator
 
 import colex
-from actus import info, warn, LogSection
+from actus import info, warn, error, LogSection, Style
 
 
 NEWLINE = "\n"
 DOT = "."
 
-match_log = LogSection("Match")
+match_log = LogSection("Match", style=Style(
+    label=colex.CRIMSON
+))
 
 
 class ParserArguments(argparse.Namespace):
@@ -25,54 +27,13 @@ class ParserArguments(argparse.Namespace):
     flag: int
 
 
-parser = argparse.ArgumentParser(
-    prog="grep",
-    description="Know which file and where, that contains specified regular expression"
-)
-parser.add_argument("pattern")
-parser.add_argument("-v", "--version",
-                    action="version",
-                    version=f"%(prog)s: v{__version__}")
-parser.add_argument("-p", "--path",
-                    default=".",
-                    help="Path to initial search location")
-parser.add_argument("-r", "--depth",
-                    dest="depth",
-                    metavar="[depth]",
-                    type=int,
-                    default=1,
-                    nargs="?",
-                    help="Recurision depth. Defaults to 1")
-parser.add_argument("-i", "--include-dotfile",
-                    dest="include_dotfile",
-                    action="store_true",
-                    help="Recurision depth. Defaults to 1")
-parser.add_argument("-f",
-                    dest="flag",
-                    metavar="[flag]",
-                    action="store",
-                    type=int,
-                    default=0,
-                    help="Compiler flag for regular expression")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("--verbose",
-                    action="store_true",
-                    help="Display more information during execution")
-group.add_argument("--silent",
-                    action="store_true",
-                    help="Display less information during execution")
-args = ParserArguments()
-parser.parse_args(namespace=args)
-
-absolute_path = (pathlib.Path
-                 .cwd()
-                 .joinpath(args.path)
-                 .resolve())
-
-pattern = re.compile(args.pattern, flags=args.flag)
-
-
-def process_file(file_path: pathlib.Path) -> None:
+def process_file(
+    file_path: pathlib.Path,
+    /,
+    *,
+    pattern: re.Pattern[str],
+    args: ParserArguments
+) -> None:
     try:
         content = pathlib.Path(file_path).read_text(encoding="utf-8")
     except UnicodeDecodeError as error:
@@ -105,7 +66,10 @@ def process_file(file_path: pathlib.Path) -> None:
 
 def walk_paths(
     start: pathlib.Path,
-    depth: int | None = None
+    /,
+    *,
+    depth: int | None = None,
+    args: ParserArguments
 ) -> Generator[pathlib.Path, None, None]:
     if depth is not None:
         if depth <= 0:
@@ -116,23 +80,83 @@ def walk_paths(
             if not args.include_dotfile:
                 continue
         if sub_path.is_file():
-            # try: # check if can be opened using utf-8 decoding
-            #     fd = open(sub_path, "r", encoding="utf-8")
-            # except UnicodeDecodeError:
-            #     print("FAILED")
-            #     continue
-            # finally:
-            #     fd.close()
             yield sub_path
         else:
-            yield from walk_paths(sub_path, depth=depth)
+            yield from walk_paths(sub_path, depth=depth, args=args)
 
 
-assert absolute_path.exists()
+def main() -> int:
+    parser = argparse.ArgumentParser(
+        prog="grep",
+        description="Know which file and where, that contains specified regular expression"
+    )
+    parser.add_argument("pattern")
+    parser.add_argument(
+        "-v", "--version",
+        action="version",
+        version=f"%(prog)s: v{__version__}"
+    )
+    parser.add_argument(
+        "-p", "--path",
+        default=".",
+        help="Path to initial search location"
+    )
+    parser.add_argument(
+        "-r", "--depth",
+        dest="depth",
+        metavar="[depth]",
+        type=int,
+        default=1,
+        nargs="?",
+        help="Recurision depth. Defaults to 1"
+    )
+    parser.add_argument(
+        "-i", "--include-dotfile",
+        dest="include_dotfile",
+        action="store_true",
+        help="Recurision depth. Defaults to 1"
+    )
+    parser.add_argument(
+        "-f", "--flags",
+        dest="flag",
+        metavar="[flag]",
+        action="store",
+        type=int,
+        default=0,
+        help="Compiler flag for regular expression"
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Display more information during execution"
+    )
+    group.add_argument(
+        "--silent",
+        action="store_true",
+        help="Display less information during execution"
+    )
+    args = ParserArguments()
+    parser.parse_args(namespace=args)
 
-if absolute_path.is_file():
-    process_file(absolute_path)
-else:
-    for sub_path in walk_paths(absolute_path, depth=args.depth):
-        if sub_path.is_file():
-            process_file(sub_path)
+    absolute_path = (
+        pathlib.Path
+        .cwd()
+        .joinpath(args.path)
+        .resolve()
+    )
+
+    if not absolute_path.exists():
+        error(f"Could not find path $[{absolute_path}]")
+        info(f"Caused by flag $[-p]/$[--path]")
+        return 1 # requires valid root path
+
+    pattern = re.compile(args.pattern, flags=args.flag)
+
+    if absolute_path.is_file():
+        process_file(absolute_path, pattern=pattern, args=args)
+    else:
+        for sub_path in walk_paths(absolute_path, depth=args.depth, args=args):
+            if sub_path.is_file():
+                process_file(sub_path, pattern=pattern, args=args)
+    return 0
